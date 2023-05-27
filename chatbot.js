5c7673ea490d2605fd88420a7098bccb8a9b7907
bharadwajswarna2 <106751187+bharadwajswarna2@users.noreply.github.com>
2023-05-28 00:21:09 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -259,7 +259,7 @@ window.addEventListener('DOMContentLoaded', function () {
         $(document).ready(function() {
           handleReload()
           $('#myPopup').hide();
-           setSessionId("S-1234", 1)
+//            setSessionId("S-1234", 1)
       })
     
       // You can initialize and interact with your chatbot here
